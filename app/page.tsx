6cb9bbbbbdd0bf89f3095dac0768f307e071999f
Coke <acokenour@gmail.com>
2026-05-02 17:31:10 -0500

    Dated font.

--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,7 @@ export default async function Home({ searchParams }: { searchParams: SearchParam
         padding: "2rem",
       }}
     >
-      <h1 style={{ marginBottom: "1rem", fontWeight: 300 }}>
+      <h1 style={{ marginBottom: "1rem", fontWeight: 700 }}>
         It's all blue baby
       </h1>
       <p
