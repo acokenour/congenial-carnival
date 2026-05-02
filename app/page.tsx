fcc03300e18d329ef51fe69762c64b2a98dd8918
Coke <acokenour@gmail.com>
2026-05-02 17:31:30 -0500

    Dialing changes.

--- app/page.tsx
+++ app/page.tsx
@@ -33,9 +33,9 @@ export default async function Home({ searchParams }: { searchParams: SearchParam
         padding: "2rem",
       }}
     >
-      <h1 style={{ marginBottom: "1rem", fontWeight: 700 }}>
-        It's all blue baby
-      </h1>
+     <h1 style={{ marginBottom: "1rem", fontWeight: 700, letterSpacing: "0.08em", textTransform: "uppercase" }}>
+  It's all blue baby
+</h1>
       <p
         style={{
           marginBottom: "2rem",
